cc3f4024abe06a89cd62bc5af405ea4743776176
Johnattas Santana <johnattas@gmail.com>
2026-05-18 16:10:45 -0300

    feat: atualizando componentes

--- dist/Composables/useRefCached.d.ts
+++ dist/Composables/useRefCached.d.ts
@@ -1,5 +1,6 @@
 import { Ref } from 'vue';
-export declare function useRefCached<T>(key: string, default_value: T): Ref<T>;
+export type RefCached<T> = Ref<T>;
+export declare function useRefCached<T>(key: string, default_value: T): RefCached<T>;
 export declare const useRefStorage: typeof useRefCached;
 export declare const useCached: typeof useRefCached;
 export declare const useSharedCache: typeof useRefCached;
--- src/Composables/useRefCached.ts
+++ src/Composables/useRefCached.ts
@@ -3,10 +3,12 @@ import { watchDebounced, useStorage as vueUseStorage } from '@vueuse/core';
 
 import { type Ref } from 'vue';
 
-export function useRefCached<T>(key: string, default_value: T): Ref<T> {
+export type RefCached<T> = Ref<T>;
+
+export function useRefCached<T>(key: string, default_value: T): RefCached<T> {
     localforage.config({ name: 'caches', storeName: 'use-ref-storages' });
 
-    const state = vueUseStorage<T>(key, default_value) as unknown as Ref<T>;
+    const state = vueUseStorage<T>(key, default_value) as unknown as RefCached<T>;
 
     localforage.getItem(key).then((value: any) => state.value = value ? value : default_value);
     watchDebounced(state, () => localforage.setItem(key, JSON.parse(JSON.stringify(state.value))), { debounce: 600 });
