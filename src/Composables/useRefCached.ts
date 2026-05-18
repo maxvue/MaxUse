fa060933f17b5b4b42731b10ab51e3816c7d9128
Johnattas Santana <johnattas@gmail.com>
2026-05-18 16:18:13 -0300

    feat: atualizando componentes

--- src/Composables/useRefCached.ts
+++ src/Composables/useRefCached.ts
@@ -1,10 +1,11 @@
 import localforage from 'localforage';
 import { watchDebounced, useStorage as vueUseStorage, type RemovableRef } from '@vueuse/core';
+import type { Ref } from 'vue';
 
-export function useRefCached<T>(key: string, default_value: T): RemovableRef<T> {
+export function useRefCached<T>(key: string, default_value: T): Ref<T> {
     localforage.config({ name: 'caches', storeName: 'use-ref-storages' });
 
-    const state = vueUseStorage(key, default_value);
+    const state: Ref<T> = vueUseStorage(key, default_value);
 
     localforage.getItem(key).then((value: any) => state.value = value ? value : default_value);
     watchDebounced(state, () => localforage.setItem(key, JSON.parse(JSON.stringify(state.value))), { debounce: 600 });
