52c238949d8dd162f36ce6d11ab02aa4e0b745d8
Johnattas Santana <johnattas@gmail.com>
2026-05-18 17:02:14 -0300

    feat: atualizando componentes

--- dist/Composables/useRefCached.d.ts
+++ dist/Composables/useRefCached.d.ts
@@ -1,9 +1,8 @@
 import { Ref } from 'vue';
-type CachedRef<T> = Ref<T, T | null | undefined>;
-export declare function useRefCached<T>(key: string, default_value: T): CachedRef<T>;
+export type ToRefCached<T> = T extends Ref ? T : Ref<T>;
+export declare function useRefCached<T>(key: string, default_value: T): ToRefCached<T>;
 export declare const useRefStorage: typeof useRefCached;
 export declare const useCached: typeof useRefCached;
 export declare const useSharedCache: typeof useRefCached;
 export declare const useStorage: typeof useRefCached;
-export { CachedRef };
 //# sourceMappingURL=useRefCached.d.ts.map
--- src/Composables/useRefCached.ts
+++ src/Composables/useRefCached.ts
@@ -2,12 +2,13 @@ import localforage from 'localforage';
 import { watchDebounced } from '@vueuse/core';
 import { ref, Ref } from 'vue';
 
-type CachedRef<T> = Ref<T, T | null | undefined>;
+export type ToRefCached<T> = T extends Ref ? T : Ref<T>;
 
-export function useRefCached<T>(key: string, default_value: T):CachedRef<T> {
+
+export function useRefCached<T>(key: string, default_value: T): ToRefCached<T> {
     localforage.config({ name: 'caches', storeName: 'use-ref-storages' });
 
-    const state = ref(default_value) as CachedRef<T>;
+    const state = ref(default_value) as ToRefCached<T>;
 
     localforage.getItem(key).then((value: any) => state.value = value ? value : default_value);
     watchDebounced(state, () => localforage.setItem(key, JSON.parse(JSON.stringify(state.value))), { debounce: 600 });
@@ -19,5 +20,3 @@ export const useRefStorage = useRefCached;
 export const useCached = useRefCached;
 export const useSharedCache = useRefCached;
 export const useStorage = useRefCached;
-
-export { CachedRef };
