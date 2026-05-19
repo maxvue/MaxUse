3fd8126795a548350ddbf68f15dd5dc8e61ea356
Johnattas Santana <johnattas@gmail.com>
2026-05-19 14:35:21 -0300

    feat: atualizando componentes

--- dist/Composables/useDefaultReset.d.ts
+++ dist/Composables/useDefaultReset.d.ts
@@ -4,7 +4,7 @@ export interface DefaultResetExtends<T> extends Ref<T> {
     initialData: string;
     timer?: number | null;
 }
-export type DefaultReset<T> = [T] extends [DefaultResetExtends<T>] ? T : DefaultResetExtends<T> | null;
+export type DefaultReset<T> = [T] extends [DefaultResetExtends<T>] ? false | T : DefaultResetExtends<T> | null;
 export declare function useDefaultReset<T>(initialData: T, timer?: number | null): DefaultReset<T>;
 export declare const refAutoReset: typeof useDefaultReset;
 //# sourceMappingURL=useDefaultReset.d.ts.map
--- src/Composables/useDefaultReset.ts
+++ src/Composables/useDefaultReset.ts
@@ -3,7 +3,7 @@ import { ulid } from 'ulid';
 import { watchDebounced } from '@vueuse/core';
 
 export interface DefaultResetExtends<T> extends Ref<T> { reset(): void; initialData: string; timer?: number | null };
-export type DefaultReset<T> = [T] extends [DefaultResetExtends<T>] ? T : DefaultResetExtends<T> | null;
+export type DefaultReset<T> = [T] extends [DefaultResetExtends<T>] ? false | T : DefaultResetExtends<T> | null;
 
 export function useDefaultReset<T>(initialData: T, timer: number | null = null): DefaultReset<T> {
 
