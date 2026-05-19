4ec18308ff286571602e1f96b2407279e916d910
Johnattas Santana <johnattas@gmail.com>
2026-05-19 13:54:09 -0300

    feat: atualizando componentes

--- src/Composables/useDefaultReset.ts
+++ src/Composables/useDefaultReset.ts
@@ -3,11 +3,11 @@ import { ulid } from 'ulid';
 import { watchDebounced } from '@vueuse/core';
 
 export type Reset = { reset(): void; initialData: string; timer?: number | null };
-export type DefaultReset<T> = (T extends Ref ? T : Ref<T>) & Reset;
+export type DefaultReset<T> = ([T] extends [Ref] ? T : Ref<T>) & Reset;
 
 export function useDefaultReset<T>(initialData: T, timer: number | null = null): DefaultReset<T> {
 
-    const state = ref(initialData as T) as DefaultReset<T>;
+    const state = ref<T>(initialData) as DefaultReset<T>;
     state.initialData = JSON.stringify(initialData);
 
     state.reset = () => {
