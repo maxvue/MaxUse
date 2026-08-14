ecdc8a4ea8ca768996a5f84248c44e37e2bd2eac
Johnattas Santana <johnattas@gmail.com>
2026-08-14 08:38:19 -0300

    fix: reutiliza instancia de Intl.NumberFormat em formatCurrency (#10)

--- src/Helpers/Format/currency.ts
+++ src/Helpers/Format/currency.ts
@@ -5,6 +5,37 @@ import { parseBrNumber } from '../Strings/converters';
 type RefString = MaybeRefOrGetter<string | number | null | undefined>;
 
 /**
+ * Locale e opções do formatador de moeda.
+ *
+ * São constantes: `formatCurrency` não aceita locale nem opções por parâmetro,
+ * portanto existe uma única configuração possível e a instância pode ser
+ * compartilhada com segurança. Caso algum dia a função passe a receber opções
+ * variáveis, este singleton precisa virar um cache com chave que cubra TODAS
+ * as opções que afetam a saída (locale, currency, dígitos, etc.).
+ */
+const BRL_LOCALE = 'pt-BR';
+
+const BRL_OPTIONS: Intl.NumberFormatOptions = {
+    style: 'currency',
+    currency: 'BRL'
+};
+
+/**
+ * Instância única do `Intl.NumberFormat`, criada de forma preguiçosa.
+ *
+ * Construir um `Intl.NumberFormat` custa ~26 µs; reaproveitar a instância evita
+ * milissegundos de main thread em listas grandes. A inicialização preguiçosa
+ * mantém o módulo livre de efeito colateral no import (tree-shaking).
+ */
+let brlFormatter: Intl.NumberFormat | null = null;
+
+function getBrlFormatter(): Intl.NumberFormat {
+    if (!brlFormatter) brlFormatter = new Intl.NumberFormat(BRL_LOCALE, BRL_OPTIONS);
+
+    return brlFormatter;
+}
+
+/**
  * Formata um número para o padrão de moeda brasileira (R$).
  *
  * @param value O valor a ser formatado.
@@ -16,9 +47,6 @@ export function formatCurrency(value: RefString): string {
     const num = parseBrNumber(data);
     if (isNaN(num)) return 'R$ 0,00';
 
-    return new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    }).format(num).replace(/[\u00a0\u202f]/g, ' ');
+    return getBrlFormatter().format(num).replace(/[\u00a0\u202f]/g, ' ');
 }
 
